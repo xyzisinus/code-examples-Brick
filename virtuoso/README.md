813b0e7b78d3e7a6f37488b55243a0918fac8c82
Xiaolin (Charlene) Zang <charlene@xzang.net>
2020-02-11 18:40:58 +0000

    checkpoint graph db demon file.

--- virtuoso/README.md
+++ virtuoso/README.md
@@ -1,7 +1,8 @@
 `connectDB.py`: select/insert/delete triples using the sparqlWrapper package as database user dba.  Do the following
-to grant permissions to `SPARQL` using Virtuoso's web interface `DBhost:8890`. 
+to grant permissions to `SPARQL` using Virtuoso's web interface `DBhost:8890`.
 ```
 grant execute on "DB.DBA.SPARQL_INSERT_DICT_CONTENT" to "SPARQL";
 grant execute on "DB.DBA.SPARQL_DELETE_DICT_CONTENT" to "SPARQL";
 grant execute on "DB.DBA.SPARQL_MODIFY_BY_DICT_CONTENTS" to "SPARQL";
+grant execute on "DB.DBA.SPARUL_LOAD" to "SPARQL";
 ```
--- virtuoso/connectDB.py
+++ virtuoso/connectDB.py
@@ -13,6 +13,10 @@ import sys
 sys.path.append('..')
 from creds import virtuosoCreds
 
+# Demonstrate grap db operations via SPARQLWrapper.
+# The ops here need permissions granted to "sparql" by virtuoso db.
+# See README.md for details.
+
 defaultGraph = 'http://www.example.org/graph-selected'
 
 def getSparql(update=False):
@@ -25,6 +29,51 @@ def getSparql(update=False):
     if update:
         sparql.setMethod(POST)
     return sparql
+
+def queryGraph(details=False):
+    sparql = getSparql()
+    sparql.setQuery('WITH <http://www.example.org/graph-selected> SELECT * WHERE { ?s ?p ?o. }')
+    ret = sparql.query().convert()
+    triples = ret['results']['bindings']
+    print(f'# of triples in {defaultGraph}', len(triples))
+
+    if not details: return
+    for r in triples:
+        print('<%s> <%s> <%s>' % (r['s']['value'], r['p']['value'], r['o']['value']))
+
+def deleteAll():
+    print('delete all triples')
+    try:
+        sparql = getSparql(update=True)
+        sparql.setQuery("""
+        WITH <http://www.example.org/graph-selected>
+        DELETE { ?s ?p ?o } WHERE { ?s ?p ?o . }
+        """)
+        results = sparql.query()
+    except Exception as e:
+        print('delete all exception %s' % e)
+
+    queryGraph()
+
+def loadFileViaURL():
+    print('load file via URL')
+    try:
+        sparql = getSparql(update=True)
+        sparql.setQuery("""
+        WITH <http://www.example.org/graph-selected>
+        LOAD <https://brickschema.org/schema/1.0.3/Brick.ttl> INTO <http://www.example.org/graph-selected>
+        """)
+        results = sparql.query()
+    except Exception as e:
+        print('load file via URL exception %s' % e)
+
+    queryGraph()
+
+queryGraph()
+loadFileViaURL()
+queryGraph(details=True)
+deleteAll()
+
 
 # try insert: must exec
 # grant execute on "DB.DBA.SPARQL_INSERT_DICT_CONTENT" to "SPARQL";
