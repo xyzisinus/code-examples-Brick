b23408b038d4efda1a593fc051821052e863e89b
Xiaolin (Charlene) Zang <charlene@xzang.net>
2020-03-06 04:46:11 +0000

    Add tests

--- graph_db_wrapper/brickEndpoint.py
+++ graph_db_wrapper/brickEndpoint.py
@@ -214,30 +214,3 @@ class BrickEndpoint():
             raise e
 
 # end of BrickEndpoint()
-
-if __name__ == '__main__':
-    defaultGraph = 'http://www.xyz.abc/graph'
-    ep = BrickEndpoint('http://localhost:8890/sparql',
-                       '1.0.3',
-                       defaultGraph,
-                       loadSchema=False)
-    ep.listGraphs()
-    ep.loadFileViaURL(ep.Brick)
-    ep.dropGraph(ep.Brick, force=True)
-
-    # load all schema files
-    ep.loadSchema()
-    ep.listGraphs()
-
-    ep.createGraph('http://abc.efg')
-    ep.loadFileViaURL(ep.Brick, graphName='http://abc.efg')
-    ep.listGraphs()
-    ep.dropGraph('http://abc.efg')
-    ep.listGraphs()
-
-    g = Graph()
-    g.parse('sample_graph.ttl', format='turtle')
-    ep.loadGraph(g, defaultGraph)
-    resultG = ep.queryGraph(defaultGraph, verbose=True)
-    ep.dropGraph(defaultGraph, force=True)
-    ep.listGraphs()
--- tests/test_BrickEndpoint.py
+++ tests/test_BrickEndpoint.py
@@ -1,10 +1,32 @@
 from graph_db_wrapper.brickEndpoint import BrickEndpoint
 
+defaultGraph = 'http://www.xyz.abc/graph'
+
 def test_xxx():
     print("in test")
-    defaultGraph = 'http://www.xyz.abc/graph'
     ep = BrickEndpoint('http://localhost:8890/sparql',
                        '1.0.3',
                        defaultGraph,
                        loadSchema=False)
+    ep.listGraphs()
+    ep.loadFileViaURL(ep.Brick)
+    ep.dropGraph(ep.Brick, force=True)
+
+    # load all schema files
+    ep.loadSchema()
+    ep.listGraphs()
+
+    ep.createGraph('http://abc.efg')
+    ep.loadFileViaURL(ep.Brick, graphName='http://abc.efg')
+    ep.listGraphs()
+    ep.dropGraph('http://abc.efg')
+    ep.listGraphs()
+
+    return
+
+    g = Graph()
+    g.parse('sample_graph.ttl', format='turtle')
+    ep.loadGraph(g, defaultGraph)
+    resultG = ep.queryGraph(defaultGraph, verbose=True)
+    ep.dropGraph(defaultGraph, force=True)
     ep.listGraphs()
